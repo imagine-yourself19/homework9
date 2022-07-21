a122ee3e0ee6c0d6640b048eb0c9f8909ae41444
Lena <imagine.youeself19@gmail.com>
2022-07-21 12:27:03 +0300

    решена задача 66

---
+++ задача66/Program.cs
@@ -0,0 +1,17 @@
+﻿void SumOfNumbers(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма чисел от m до n равна {sum}");
+        return;
+    }
+    sum += m++;
+    SumOfNumbers(m, n, sum);
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+SumOfNumbers(m, n, sum);
