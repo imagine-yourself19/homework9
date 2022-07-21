64a7985134dcddc01623c59e7155c360f72156ee
Lena <imagine.youeself19@gmail.com>
2022-07-21 12:27:14 +0300

    решена задача 68

---
+++ задача68/Program.cs
@@ -0,0 +1,14 @@
+﻿int AckermannFunction(int n, int m)
+{
+    if (n == 0)
+        return (m + 1);
+    if (m == 0)
+        return AckermannFunction(n - 1, 1);
+    return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Значение функции Аккермана {AckermannFunction(n, m)}");
