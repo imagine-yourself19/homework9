bf0dca880c406ce469cb6027923c0d425e3dff8d
Lena <imagine.youeself19@gmail.com>
2022-07-22 11:47:43 +0300

    решена задача 64

---
+++ задача64/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintNumbers(int n, int m)
+{
+    if (n != (m-1))
+    {
+        PrintNumbers(n - 1, m);
+        Console.Write(n + " ");
+    }
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(n, m);
